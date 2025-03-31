84348ee25c1798505dacd47074608455d08b72af
Gabriel Silveira <bsi.silveira@gmail.com>
2025-03-31 08:42:30 -0300

    chore: :wrench: Update Core app configuration and add new dependencies for Django
    #1

--- backend/settings.py
+++ backend/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "drf_spectacular",
+    "safedelete",
+    "simple_history",
+    "backend.core",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "simple_history.middleware.HistoryRequestMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
